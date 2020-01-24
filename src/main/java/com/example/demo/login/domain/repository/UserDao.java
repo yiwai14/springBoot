caf2bbed28647c1e9cd99cbaea9f82c84f6326a9
yiwai14 <mosyaking2018@gmail.com>
2020-01-24 14:57:56 +0900

    save

---
+++ src/main/java/com/example/demo/login/domain/repository/UserDao.java
@@ -0,0 +1,5 @@
+package com.example.demo.login.domain.repository;
+
+public class UserDao {
+
+}
---
+++ src/main/java/com/example/demo/login/domain/repository/jdbc/UserDaoJdbcImpl.java
@@ -0,0 +1,5 @@
+package com.example.demo.login.domain.repository.jdbc;
+
+public class UserDaoJdbcImpl {
+
+}
